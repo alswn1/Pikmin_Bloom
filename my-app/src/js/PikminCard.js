eb887901d459090abd804d97f6d29d9c7cc7e17d
alswn1 <alswn5790@naver.com>
2024-11-14 14:13:17 +0900

    디자인 수정 및 pikmin 화면 개발

--- my-app/src/js/PikminCard.js
+++ my-app/src/js/PikminCard.js
@@ -4,15 +4,7 @@ import '../css/PikminCard.css';
 const PikminCard = ({ pikmin }) => {
     return (
         <div className="col">
-            <img className="pik_img" src={pikmin?.img} />
-            <div className="card-contain">
-                <div>{pikmin?.name}</div><br/>
-                <div className="des">{pikmin?.description}</div><br/>
-                <div>운반 : {pikmin?.carriage}</div>
-                <div>공격 : {pikmin?.strength}</div>
-                <div>성장 : {pikmin?.growth}</div>
-                <div>저항 : {pikmin?.resistance}</div>
-            </div>
+            <img className="pik-img" src={`http://localhost:5000${pikmin.front_img}`}  alt={pikmin.name} />
         </div>
     );
 };
